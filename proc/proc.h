380bc34f9620a3d96f5d28a7d68b78e5427d1a5f
ghostapple <ghost@ghost_station>
2016-09-02 21:27:59 +0800

    Half-finish simple double link list and process related data structures.

--- proc/proc.h
+++ proc/proc.h
@@ -1,39 +1,6 @@
 #ifndef _PROC_H_
 #define _PROC_H_
-struct namespace
-{
-	
-	
-}
-/*
- * Session consists of a set of process groups,
- * associating with a user and maybe a terminal device.
- */
-struct session
-{
-	
-	
-}
-/*
- * Process groups may have a or more processes.
- * Each process associates with a process groups.
- */
-struct pgrp
-{
-	
-}
-/*
- * Process structure.
- */
-struct proc
-{
-	struct mtx	p_mtx;	//Process struct lock
-	pid_t		pid;	//Process ID
-	
-	
-	
-	
-}
+
 /*
  * Kernel runnable context (thread).
  * This is what is put to sleep and reactivated.
@@ -41,16 +8,64 @@ struct proc
  */
 struct thread
 {
-	struct mtx	t_mtx;	//Thread struct lock
-	tid_t		tid;	//Thread ID
+	struct list_head	t_list;	//List of all threads
+	struct mtx		t_mtx;	//Thread struct lock
+	tid_t			t_tid;	//Thread ID
 	
-}
+};
 /*
  * pargs, used to hold a copy of the command line, if it had a sane length.
  */
-struct pargs {
+struct pargs 
+{
 	uint	ar_ref;		/* Reference count. */
 	uint	ar_length;	/* Length. */
 	uchar	*ar_args;	/* Arguments. */
+};
+/*
+ * Process structure.
+ */
+struct proc
+{
+	struct list_head	p_plist;	//List of all processes
+	struct thread		*p_threads;	//Thread list entry
+	struct mtx		p_mtx;		//Process struct lock
+	pid_t			p_pid;		//Process ID
+	uid_t			p_uid;		//User ID
+	struct proc		*p_parent;	//Parent Process
+	struct proc		*p_child;	//Child Process list entry
+	
+	signed char 		p_nice;		//Nice of the process
+	struct pargs		p_args;		//Arguments from command line
+	char*			p_wd;		//Current working directory
+};
+/*
+ * Process groups may have a or more processes.
+ * Each process associates with a process groups.
+ */
+struct pgrp
+{
+	struct list_head	g_glist;	//List of all process groups
+	struct proc		*g_procs;	//Process struct entry
+	gid_t			g_gid;		//Group ID
+	uid_t			g_uid;		//User ID
+};
+/*
+ * Session consists of a set of process groups,
+ * associating with a user and maybe a terminal device.
+ */
+struct session
+{
+	struct list_head	s_slist;	//List of all sessions
+	struct pgrp		*s_pgrps;	//Group struct entry
+	sid_t			s_sid;		//Session ID
+	uid_t			s_uid;		//User ID
+};
+
+struct namespace
+{
+	struct list_head	n_nlist;	//List of all namespaces
+	nid_t			n_nid;		//Namespace ID
+	uid_t			n_uid;		//User ID
 };
 #endif
