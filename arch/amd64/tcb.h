2a62317f30db209f1c6ff989ff15ff6c2c72624b
ghostapple <ghost@ghost_station>
2016-09-09 03:38:10 +0800

    Start writing context switch assembly code.

--- arch/amd64/tcb.h
+++ arch/amd64/tcb.h
@@ -11,25 +11,25 @@ struct tcb
  * General-perpose registers
  * Special uses:
  * RAX:Accumulator for operands and results data
- * RBX:Pointer to data in the DS segment
  * RCX:Counter for string and loop operations
  * RDX:I/O pointer
+ * RBX:Pointer to data in the DS segment
  * RBP:Pointer to data on the stack(in the SS segment)
+ * RSP:Stack pointer(in the SS segment)
  * RSI:Pointer to data in the segment pointed to by the DS register;
  *     source pointer for string operations
  * RDI:Pointer to data(or destination)in the segment pointed by the ES register;
  *     destination pointer for string operations
- * RSP:Stack pointer(in the SS segment)
  * 
  */
 	uint64_t RAX;
-	uint64_t RBX;
 	uint64_t RCX;
 	uint64_t RDX;
+	uint64_t RBX;
 	uint64_t RBP;
+	uint64_t RSP;
 	uint64_t RSI;
 	uint64_t RDI;
-	uint64_t RSP;
 //New general-perpose registers(GPRs)
 	uint64_t R8;
 	uint64_t R9;
@@ -40,10 +40,10 @@ struct tcb
 	uint64_t R14;
 	uint64_t R15;
 //Segment registers
-	uint64_t CS;//CS,DS,ES,SS are treated as if each
-	uint64_t DS;//segment base is 0 in 64-bit mode.
-	uint64_t ES;//FS,GS may be used as additional base
-	uint64_t SS;//registers in linear address calculations.
+//	uint64_t CS;//CS,DS,ES,SS are treated as if each
+//	uint64_t DS;//segment base is 0 in 64-bit mode.
+//	uint64_t ES;//FS,GS may be used as additional base
+//	uint64_t SS;//registers in linear address calculations.
 	uint64_t FS;
 	uint64_t GS;
 //Flags and Status register
