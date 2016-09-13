13b7c3f17852734de56a6a77d02ee9162b805748
lifeibiren <lifeibiren@qq.com>
2016-09-13 19:55:33 +0800

    Finished sched.c

--- proc/sched.c
+++ proc/sched.c
@@ -1,12 +1,36 @@
 #include "proc.h"
 
+struct run_queue
+{
+	list_head queue;
+	struct thread *thread;
+}*running;
+
+void get_current_thread_pcb(void)
+{
+	//Save old thread's pcb into %rax register.
+	asm (
+		"mov %0, %%rax"
+		:
+		: "m"(current->thread->pcb)
+		:
+	);
+}
 /**
  * This function is called after context is saved,
  * then we can schedule.
  */
 void sched(void)
 {
+	current = current->queue->next;
 	
-	
+
+	//Save new thread's pcb into %rax register.
+	asm (
+		"mov %0, %%rax"
+		:
+		: "m"(current->thread->pcb)
+		:
+	);
 	
 }
