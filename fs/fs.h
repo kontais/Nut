42b02bb085265b74d6fef574b5d6034a20a3b4b7
Li Fei <lifeibiren@qq.com>
2016-12-23 18:39:44 +0800

    Adjust fs.h

--- fs/fs.h
+++ fs/fs.h
@@ -20,48 +20,48 @@ typedef struct file_node
 	
 	struct list_head list;
 	struct list_head content;
-	file_node *parent;
+	struct file_node *parent;
 	
 	struct file_hook_t *hook;
 }file_node_t;
 
 typedef struct file_hook
 {
-	int (*open)(file_node_t *node, void **context, int oflag);
-	int (*close)(file_node_t *node, void **context);
-	int (*read)(file_node_t *node, void **context, void *buf, uint64_t size);
-	int (*write)(file_node_t *node, void **context, void *buf, uint64_t size);
-	off_t (*lseek)(file_node_t *node, void **context, off_t offset, int whence);
-	int (*ioctl)(file_node_t *node, void **context, int request, ...);
-	int (*stat)(file_node_t *node, void **context, void *buf);
+	void *(*open)(file_node_t *node, const char *path, int oflag);
+	int (*close)(file_node_t *node, void *context);
+	int (*read)(file_node_t *node, void *context, void *buf, uint64_t size);
+	int (*write)(file_node_t *node, void *context, void *buf, uint64_t size);
+	off_t (*lseek)(file_node_t *node, void *context, off_t offset, int whence);
+	int (*ioctl)(file_node_t *node, void *context, int request, ...);
+	int (*stat)(file_node_t *node, void *context, void *buf);
 }file_hook_t;
 
-typedef struct context
+typedef struct fs_context
 {
 	file_node_t *node;
 	void **context;
-}context_t;
+}fs_context_t;
 
 void fs_init(void);
 int fs_mknode(const char *path, uint64_t type);
 int fs_rmnode(const char *path);
 file_node_t *fs_getnode(const char *path);
 
-int fs_reghook_open(const char *path, int (*open)(file_node_t *node, void **context, int oflag));
-int fs_reghook_close(const char *path,int (*close)(file_node_t *node, void **context));
-int fs_reghook_read(const char *path, int (*read)(file_node_t *node, void **context, void *buf, uint64_t size));
-int fs_reghook_write(const char *path, int (*write)(file_node_t *node, void **context, void *buf, uint64_t size));
-int fs_reghook_lseek(const char *path, off_t (*lseek)(file_node_t *node, void **context, off_t offset, int whence));
-int fs_reghook_ioctl(const char *path, int (*ioctl)(file_node_t *node, void **context, int request, ...));
-int fs_reghook_stat(const char *path, int (*stat)(file_node_t *node, void **context, void *buf));
+int fs_reghook_open(const char *path, void *(*open)(file_node_t *node, const char *path, int oflag));
+int fs_reghook_close(const char *path,int (*close)(file_node_t *node, void *context));
+int fs_reghook_read(const char *path, int (*read)(file_node_t *node, void *context, void *buf, uint64_t size));
+int fs_reghook_write(const char *path, int (*write)(file_node_t *node, void *context, void *buf, uint64_t size));
+int fs_reghook_lseek(const char *path, off_t (*lseek)(file_node_t *node, void *context, off_t offset, int whence));
+int fs_reghook_ioctl(const char *path, int (*ioctl)(file_node_t *node, void *context, int request, ...));
+int fs_reghook_stat(const char *path, int (*stat)(file_node_t *node, void *context, void *buf));
 
-int fs_open(context_t **context, const char *path,, int oflag);
-int fs_close(context_t **context);
-int fs_read(context_t **context, void *buf, uint64_t size);
-int fs_write(context_t **context, void *buf, uint64_t size);
-int fs_lseek(context_t **context, off_t offset, int whence);
-int fs_ioctl(context_t **context, int request, ...);
-int fs_stat(context_t **context, void *buf);
+fs_context_t *fs_open(const char *path, int oflag);
+int fs_close(fs_context_t *context);
+int fs_read(fs_context_t *context, void *buf, uint64_t size);
+int fs_write(fs_context_t *context, void *buf, uint64_t size);
+off_t fs_lseek(fs_context_t *context, off_t offset, int whence);
+int fs_ioctl(fs_context_t *context, int request, ...);
+int fs_stat(fs_context_t *context, void *buf);
 
 
 
